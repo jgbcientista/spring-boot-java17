bf97d87a11f9b87730ad1012ff68124f1d3f9f7d
João Guedes de Brito <joao.brito@keeggo.com>
2023-09-19 15:15:58 -0300

    Ajuste: packages padroes

---
+++ src/main/java/br/com/brasilprev/mapper/PessoaDto.java
@@ -0,0 +1,12 @@
+package br.com.brasilprev.mapper;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class PessoaDto {
+
+    @NotBlank(message = "Name is mandatory")
+    private String name;
+    
+    @NotBlank(message = "Email is mandatory")
+    private String email;
+}
